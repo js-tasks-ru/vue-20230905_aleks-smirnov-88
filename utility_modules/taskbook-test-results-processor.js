9adf226c6e3077b48fa80d8790e1a0c01111f494
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-08-29 20:23:07 +0300

    Initial setup: added utility_modules/taskbook-test-results-processor.js

---
+++ utility_modules/taskbook-test-results-processor.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-console */
+
+/**
+ * @param {AggregatedResultWithoutCoverage} report
+ */
+const taskbookTestResultsProcessor = (report) => {
+  const results = [];
+
+  report.testResults.forEach((testCase) => {
+    testCase.testResults.forEach((result) => {
+      results.push({
+        description: result.title,
+        success: result.status === 'passed',
+        suite: result.ancestorTitles,
+        time: result.duration,
+      });
+    });
+  });
+
+  const ciReport = {
+    result: {
+      jest: results,
+    },
+    summary: {
+      success: report.numPassedTestSuites,
+      failed: report.numFailedTestSuites,
+    },
+  };
+
+  if (ciReport.summary.failed) {
+    console.error(JSON.stringify(ciReport));
+  } else {
+    console.log(JSON.stringify(ciReport));
+  }
+
+  return report;
+};
+
+module.exports = taskbookTestResultsProcessor;
