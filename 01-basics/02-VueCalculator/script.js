0cdf5ddcb4823cfe25a6906963a0af31c1cb0c28
Alexander Smirnov <alex-wd@yandex.ru>
2023-09-13 01:23:37 +0300

    Task 02-02 solved

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,36 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+    data() {
+        return {
+            operator: null,
+            firstOperand: null,
+            secondOperand: null
+        }
+    },
+    computed: {
+        calcResult() {
+            if (this.firstOperand !== null && this.secondOperand !== null) {
+                switch (this.operator) {
+                    case 'sum':
+                        return this.firstOperand + this.secondOperand;
+                        break;
+                    case 'subtract':
+                        return this.firstOperand - this.secondOperand;
+                        break;
+                    case 'multiply':
+                        return this.firstOperand * this.secondOperand;
+                        break;
+                    case 'divide':
+                        return this.firstOperand / this.secondOperand;
+                        break;
+                    default: 
+                    return 'Operation is not defined';
+                }
+            }
+            return 'Fill in all data';
+        }
+    }
+});
+
+app.mount('#app');
