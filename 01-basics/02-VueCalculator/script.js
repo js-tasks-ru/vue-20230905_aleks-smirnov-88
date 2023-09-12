c6c97d4e5757c87a4a82dc3b2c6b4505f907d912
Alexander Smirnov <alex-wd@yandex.ru>
2023-09-13 01:30:39 +0300

    02-02 Code Formatted

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,36 +1,36 @@
 import { createApp } from './vendor/vue.esm-browser.js';
 
 const app = createApp({
-    data() {
-        return {
-            operator: null,
-            firstOperand: null,
-            secondOperand: null
-        }
-    },
-    computed: {
-        calcResult() {
-            if (this.firstOperand !== null && this.secondOperand !== null) {
-                switch (this.operator) {
-                    case 'sum':
-                        return this.firstOperand + this.secondOperand;
-                        break;
-                    case 'subtract':
-                        return this.firstOperand - this.secondOperand;
-                        break;
-                    case 'multiply':
-                        return this.firstOperand * this.secondOperand;
-                        break;
-                    case 'divide':
-                        return this.firstOperand / this.secondOperand;
-                        break;
-                    default: 
-                    return 'Operation is not defined';
-                }
-            }
-            return 'Fill in all data';
-        }
+  data() {
+    return {
+      operator: null,
+      firstOperand: null,
+      secondOperand: null
     }
+  },
+  computed: {
+    calcResult() {
+      if (this.firstOperand !== null && this.secondOperand !== null) {
+        switch (this.operator) {
+          case 'sum':
+            return this.firstOperand + this.secondOperand;
+            break;
+          case 'subtract':
+            return this.firstOperand - this.secondOperand;
+            break;
+          case 'multiply':
+            return this.firstOperand * this.secondOperand;
+            break;
+          case 'divide':
+            return this.firstOperand / this.secondOperand;
+            break;
+          default:
+            return 'Operation is not defined';
+        }
+      }
+      return 'Fill in all data';
+    }
+  }
 });
 
 app.mount('#app');
