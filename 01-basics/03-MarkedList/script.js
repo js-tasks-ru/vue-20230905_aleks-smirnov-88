5fa38e36fcd7451575e21d5655a1a73df6d7f1be
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-09-13 08:57:27 +0300

    Added task MarkedList

---
+++ 01-basics/03-MarkedList/script.js
@@ -0,0 +1,32 @@
+// import { createApp } from './vendor/vue.esm-browser.js';
+
+// From https://jsonplaceholder.typicode.com/comments
+const emails = [
+  'Eliseo@gardner.biz',
+  'Jayne_Kuhic@sydney.com',
+  'Nikita@garfield.biz',
+  'Lew@alysha.tv',
+  'Hayden@althea.biz',
+  'Presley.Mueller@myrl.com',
+  'Dallas@ole.me',
+  'Mallory_Kunze@marie.org',
+  'Meghan_Littel@rene.us',
+  'Carmen_Keeling@caroline.name',
+  'Veronica_Goodwin@timmothy.net',
+  'Oswald.Vandervort@leanne.org',
+  'Kariane@jadyn.tv',
+  'Nathan@solon.io',
+  'Maynard.Hodkiewicz@roberta.com',
+  'Christine@ayana.info',
+  'Preston_Hudson@blaise.tv',
+  'Vincenza_Klocko@albertha.name',
+  'Madelynn.Gorczany@darion.biz',
+  'Mariana_Orn@preston.org',
+  'Noemie@marques.me',
+  'Khalil@emile.co.uk',
+  'Sophia@arianna.co.uk',
+  'Jeffery@juwan.us',
+  'Isaias_Kuhic@jarrett.net',
+];
+
+// Требуется создать Vue приложение
