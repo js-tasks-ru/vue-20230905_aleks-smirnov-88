22223ade04735d6924bb3dfdcc782f044df322fb
Alexander Smirnov <alex-wd@yandex.ru>
2023-09-13 12:15:39 +0300

    02-04 solved

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,20 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const app = createApp({
+  data() {
+    return {
+      meetupId: null,
+      meetupTitle: 'Choose meetup id...'
+    }
+  },
+  watch: {
+    meetupId(val) {
+      const meetupById = fetchMeetupById(val).then(({ title }) => {
+        this.meetupTitle = title;
+      });
+    }
+  }
+});
+
+app.mount('#app');
