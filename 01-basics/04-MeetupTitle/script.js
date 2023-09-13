e766a2ae5cbd8cd739cf875191a82aeb830454dd
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-09-13 11:48:00 +0300

    Added task MeetupTitle

---
+++ 01-basics/04-MeetupTitle/script.js
@@ -0,0 +1,17 @@
+// import { createApp } from './vendor/vue.esm-browser.js';
+
+const API_URL = 'https://course-vue.javascript.ru/api';
+
+function fetchMeetupById(meetupId) {
+  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      return response.json().then((error) => {
+        throw error;
+      });
+    }
+  });
+}
+
+// Требуется создать Vue приложение
